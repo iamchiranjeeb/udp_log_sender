daab6bf4b626bce3475db743a5861359ee2d223f
iamchiranjeeb <chandanchiranjeebsahoo@gmail.com>
2025-02-13 15:55:06 +0530

    Init Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/iamchiranjeeb/udp_log_sender
+
+go 1.23.5
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func SendLogs() {
+	// Define the receiver's address
+	receiverAddr := "127.0.0.1:8080" // Change as needed
+	conn, err := net.Dial("udp", receiverAddr)
+	if err != nil {
+		fmt.Println("Error connecting to UDP server:", err)
+		return
+	}
+	defer conn.Close()
+
+	// Logs to send
+	logs := []string{
+		"Log 1: Application started",
+		"Log 2: User logged in",
+		"Log 3: Error occurred",
+	}
+
+	for _, log := range logs {
+		// Send each log
+		_, err := conn.Write([]byte(log))
+		if err != nil {
+			fmt.Println("Error sending log:", err)
+			os.Exit(1)
+		}
+		fmt.Println("Sent:", log)
+	}
+}
